e83d3c15b179e410886d5c70a7f763ef04a65f4a
cywu <wu.chgyen@gmail.com>
2023-09-28 16:40:40 +0200

    Init the repo

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+func main() {
+	proxywasm.SetVMContext(&vmContext{})
+}
+
+type vmContext struct {
+	types.DefaultVMContext
+}
+
+type pluginContext struct {
+	types.DefaultPluginContext
+}
+
+type tcpContext struct {
+	types.DefaultTcpContext
+}
